1081cc114a9d5e2c115fdf751410e8317c9a1e97
Zooty McFruity <zooty6@gmail.com>
2017-11-25 21:17:39 +0100

    updated database diagram

--- database.sql
+++ database.sql
@@ -5,94 +5,98 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema FoxyFace
+-- Schema foxyface
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema FoxyFace
+-- Schema foxyface
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `FoxyFace` DEFAULT CHARACTER SET utf8 ;
-USE `FoxyFace` ;
+CREATE SCHEMA IF NOT EXISTS `foxyface` DEFAULT CHARACTER SET utf8 ;
+USE `foxyface` ;
 
 -- -----------------------------------------------------
--- Table `FoxyFace`.`User`
+-- Table `foxyface`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FoxyFace`.`User` (
-  `Users_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `foxyface`.`user` (
+  `Users_id` INT(11) NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(128) NOT NULL,
+  `email` VARCHAR(128) NOT NULL,
   PRIMARY KEY (`Users_id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `FoxyFace`.`Post`
+-- Table `foxyface`.`post`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FoxyFace`.`Post` (
-  `Post_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  `title` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(45) NOT NULL,
-  `path` VARCHAR(45) NOT NULL,
-  `date` DATETIME NOT NULL DEFAULT NOW(),
+CREATE TABLE IF NOT EXISTS `foxyface`.`post` (
+  `Post_id` INT(11) NOT NULL,
+  `user_id` INT(11) NOT NULL,
+  `title` VARCHAR(128) NOT NULL,
+  `description` VARCHAR(255) NOT NULL,
+  `path` VARCHAR(255) NOT NULL,
+  `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`Post_id`),
   INDEX `fk_Post_1_idx` (`user_id` ASC),
   CONSTRAINT `fk_post_user_id`
     FOREIGN KEY (`user_id`)
-    REFERENCES `FoxyFace`.`User` (`Users_id`)
+    REFERENCES `foxyface`.`user` (`Users_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `FoxyFace`.`Rating`
+-- Table `foxyface`.`comment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FoxyFace`.`Rating` (
-  `Rating_id` INT NOT NULL,
-  `post_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  `stars` INT NOT NULL,
-  PRIMARY KEY (`Rating_id`, `user_id`),
-  INDEX `fk_Rating_post_id_idx` (`post_id` ASC),
-  INDEX `fk_Rating_user_id_idx` (`user_id` ASC),
-  CONSTRAINT `fk_Rating_post_id`
-    FOREIGN KEY (`post_id`)
-    REFERENCES `FoxyFace`.`Post` (`Post_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Rating_user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `FoxyFace`.`User` (`Users_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `FoxyFace`.`Comment`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FoxyFace`.`Comment` (
-  `Comment_id` INT NOT NULL,
-  `post_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `foxyface`.`comment` (
+  `Comment_id` INT(11) NOT NULL,
+  `post_id` INT(11) NOT NULL,
+  `user_id` INT(11) NOT NULL,
   `text` TEXT NOT NULL,
-  `date` DATETIME NOT NULL DEFAULT NOW(),
+  `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`Comment_id`),
   INDEX `fk_Comment_user_id_idx` (`user_id` ASC),
   INDEX `fk_Comment_post_id_idx` (`post_id` ASC),
   CONSTRAINT `fk_Comment_post_id`
     FOREIGN KEY (`post_id`)
-    REFERENCES `FoxyFace`.`Post` (`Post_id`)
+    REFERENCES `foxyface`.`post` (`Post_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Comment_user_id`
     FOREIGN KEY (`user_id`)
-    REFERENCES `FoxyFace`.`User` (`Users_id`)
+    REFERENCES `foxyface`.`user` (`Users_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `foxyface`.`rating`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `foxyface`.`rating` (
+  `Rating_id` INT(11) NOT NULL,
+  `post_id` INT(11) NOT NULL,
+  `user_id` INT(11) NOT NULL,
+  `stars` INT(11) NOT NULL,
+  PRIMARY KEY (`Rating_id`, `user_id`),
+  INDEX `fk_Rating_post_id_idx` (`post_id` ASC),
+  INDEX `fk_Rating_user_id_idx` (`user_id` ASC),
+  CONSTRAINT `fk_Rating_post_id`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `foxyface`.`post` (`Post_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Rating_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `foxyface`.`user` (`Users_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
