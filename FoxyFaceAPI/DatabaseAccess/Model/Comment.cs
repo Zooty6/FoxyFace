6ff8b4811434dd4f154ecab7c97dff2d4e4ebddf
Michael Weinberger <lyze@owl.sh>
2017-11-25 21:02:18 +0100

    Added two types from database.

---
+++ FoxyFaceAPI/DatabaseAccess/Model/Comment.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Google.Protobuf.WellKnownTypes;
+
+namespace DatabaseAccess.Model
+{
+    public class Comment
+    {
+        public int Id { get; }
+        public Post Post { get; set; }
+        public User User { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+        public Comment(int id, Post post, User user, string text, DateTime date = new DateTime())
+        {
+            Id = id;
+            Post = post;
+            User = user;
+            Text = text;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Post)}: {Post}, {nameof(User)}: {User}, {nameof(Text)}: {Text}, {nameof(Date)}: {Date}";
+        }
+
+        protected bool Equals(Comment other)
+        {
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Comment) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+    }
+}
---
+++ FoxyFaceAPI/DatabaseAccess/Model/Rating.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DatabaseAccess.Model
+{
+    public class Rating
+    {
+        public int Id { get; }
+        public Post Post { get; set; }
+        public User User { get; set; }
+
+        private int stars;
+        public int Stars
+        {
+            get => Stars;
+            set
+            {
+                if (value < 0 || value > 5)
+                {
+                    throw new ArgumentException("Stars can only be between 0 and 5");
+                }
+                stars = value;
+            }
+        }
+
+        public Rating(int id, Post post, User user)
+        {
+            Id = id;
+            Post = post;
+            User = user;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Post)}: {Post}, {nameof(User)}: {User}, {nameof(Stars)}: {Stars}";
+        }
+
+        protected bool Equals(Rating other)
+        {
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Rating) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+    }
+}
