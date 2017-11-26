ca2bfd0e43faec770b5d221954eb61127b0ffce5
Michael Weinberger <lyze@owl.sh>
2017-11-26 14:03:24 +0100

    Fixed merge conflict.

--- FoxyFaceAPI/DatabaseAccess/FoxyFaceDbManager.cs
+++ FoxyFaceAPI/DatabaseAccess/FoxyFaceDbManager.cs
@@ -33,7 +33,7 @@ namespace DatabaseAccess
             PostRepository = new PostRepository(FoxyFaceDb);
         }
 
-        public static void Initialize(string connectionString)
+        public static FoxyFaceDbManager Initialize(string connectionString)
         {
             if (instance != null)
                 throw new ArgumentException("Instance was already created");
