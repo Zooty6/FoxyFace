0f294600fc5a79151c21d5c04f94a07619232f7d
Michael Weinberger <lyze@owl.sh>
2017-12-02 19:49:28 +0100

    added pause to init app

--- initApp.bat
+++ initApp.bat
@@ -1,5 +1,5 @@
 cordova create FoxyFaceApp foxy.face.app FoxyFaceApp
 cd FoxyFaceApp
-cordova platform add browser --save
-cordova platform add android --save
+cordova platform add browser
+cordova platform add android
 PAUSE
