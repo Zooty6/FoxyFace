44a47e51c18c7d95b4271eaed0b5f6517029e8b5
Michael Weinberger <lyze@owl.sh>
2017-12-03 23:01:31 +0100

    moved to cordova events

--- FoxyFaceApp/www/js/forms.js
+++ FoxyFaceApp/www/js/forms.js
@@ -1,23 +1,25 @@
+var forms = {
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
 
+    onDeviceReady: function() {
+        $("#loginForm").ajaxForm({
+            success: function (data) {
+                if (data.hasOwnProperty("error")) {
+                    M.toast({html: data.error.description});
+                } else {
+                    var storage = window.localStorage;
+                    storage.setItem("token", data.token);
+                    window.location = "index.html";
+                }
+            },
+            error: function () {
+                alert("Couldn't send request...");
+            },
+            dataType: 'json'
+        });
+    }
+};
 
-// Login Form
-$(document).ready(function() {
-    $("#loginForm").ajaxForm({
-        success: function (data) {
-            if (data.hasOwnProperty("error")) {
-                M.toast({html: data.error.description});
-            } else {
-                var storage = window.localStorage;
-                storage.setItem("token", data.token);
-                window.location = "index.html";
-            }
-            
-            
-            
-        },
-        error: function () {
-            alert("Couldn't send request...");
-        },
-        dataType: 'json'
-    });
-});
+forms.initialize();
