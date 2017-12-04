1788ac22ebd1316a55f2d51bf2e07b8413055a1e
Michael Weinberger <lyze@owl.sh>
2017-12-04 21:28:22 +0100

    Implemented browse page.

--- FoxyFaceApp/www/js/index.js
+++ FoxyFaceApp/www/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // redirect to browse if token exists
     var storage = window.localStorage;
     if (storage.getItem("token") !== null) {
         window.location = "browse.html";
