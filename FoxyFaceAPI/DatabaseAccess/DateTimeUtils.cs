9705516d70d003f0b0fc1380c2f98fca4871a64f
Zooty McFruity <zooty6@gmail.com>
2017-12-07 00:10:27 +0100

    Finally, screw you datetime

--- FoxyFaceAPI/DatabaseAccess/DateTimeUtils.cs
+++ FoxyFaceAPI/DatabaseAccess/DateTimeUtils.cs
@@ -7,64 +7,9 @@ namespace DatabaseAccess
     {
         public static DateTime ConverTo(object datetime)
         {
-            try
-            {
-                return DateTime.ParseExact((string) datetime, "yyyy. MM. dd. HH:mm:ss",
-                    System.Globalization.CultureInfo.InvariantCulture);
-            }
-            catch (Exception)
-            {
-                try
-                {
-                    return DateTime.ParseExact((string) datetime, "dd/MM/yyyy HH:mm:ss",
-                        System.Globalization.CultureInfo.InvariantCulture);
-                }
-                catch (Exception )
-                {
-                    try
-                    {
-                        return DateTime.ParseExact((string) datetime, "yyyy-MM-dd HH:mm:ss",
-                            System.Globalization.CultureInfo.InvariantCulture);
-                    }
-                    catch (Exception)
-                    {
-                        try
-                        {
-                            return DateTime.ParseExact((string) datetime, "MM/dd/yyyy HH:mm:ss",
-                                System.Globalization.CultureInfo.InvariantCulture);
-                        }
-                        catch (Exception)
-                        {
-                            try
-                            {
-                                return DateTime.ParseExact((string) datetime, "MM/dd/yyyy hh:mm:ss tt",
-                                    System.Globalization.CultureInfo.InvariantCulture);
-                            }
-                            catch (Exception)
-                            {
-                                try
-                                {
-                                    return DateTime.ParseExact((string) datetime, "G",
-                                        System.Globalization.CultureInfo.InvariantCulture);
-                                }
-                                catch (Exception)
-                                {
-                                    try
-                                    {
-                                        if (DateTime.TryParse((string) datetime, out DateTime ret))
-                                            return ret;
-                                        throw new Exception();
-                                    }
-                                    catch (Exception)
-                                    {
-                                        throw new ArgumentException("Couldn't find proper format for datetime: " + datetime);   
-                                    }                                      
-                                }
-                            } 
-                        }
-                    }
-                }
-            }
+            if (DateTime.TryParse((string) datetime, out DateTime ret))
+                return ret;
+            throw new ArgumentException("Couldn't find proper format for datetime: " + datetime);
         }
     }
 }
